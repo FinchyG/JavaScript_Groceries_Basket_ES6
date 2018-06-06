5e48139d4d69345bd7846b1585730344325f3c64
FinchyG <lagaunt120@yahoo.com>
2018-06-06 19:06:17 +0100

    fruit foodtypes added to fruits select

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// function to capture value of selected <select> option
-
-function selected_category() {
-    var selected_foodtype = document.getElementById("food_categories").value;
-    console.log(selected_foodtype);
-}
-
 // foodtype constructor function
 
 function Foodtype(type, name, quantity, price) {
@@ -18,4 +11,18 @@ function Foodtype(type, name, quantity, price) {
 
 var banana = new Foodtype(fruit, banana, 5, "£0.20");
 var apple  = new Foodtype(fruit, apple, 5, "£0.60");
-var orange = new Foodtype(fruit, orange, 5, "£0.45";)
+var orange = new Foodtype(fruit, orange, 5, "£0.45");
+
+// function to capture value of selected <select> option
+
+function show_selected_category() {
+
+    var selected_category = document.getElementById("food_categories").value;
+
+    var available_fruits = document.getElementById("available_fruits");
+
+    if(selected_category === "fruits") {
+        available_fruits.style.display = "inline-block";
+    }
+}
+
