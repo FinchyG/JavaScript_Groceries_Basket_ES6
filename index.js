8482e50413bf9da0ce4fe967b59d77feb37c3a84
FinchyG <lagaunt120@yahoo.com>
2018-06-08 21:11:11 +0100

    + working minus banana quantity and remove banana buttons

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ function Foodtype(type, name, quantity, price) {
     this.price    = price;
 }
 
+Foodtype.prototype.total_price = function() { return this.quantity * this.price};
+
 // currently available fruits
 
-var banana = new Foodtype("fruit", "banana", 1, "£0.20");
+var banana = new Foodtype("fruit", "banana", 1, 0.20);
 var apple  = new Foodtype("fruit", "apple", 1, "£0.60");
 var orange = new Foodtype("fruit", "orange", 1, "£0.45");
 
@@ -26,18 +28,36 @@ function show_selected_category() {
     }
 }
 
-function add_banana_to_basket() {
+function add_bananas_to_basket() {
 
     var li = document.createElement("li");
     li.setAttribute("id", "banana_li");
     var ul = document.getElementById("basket");
-    
-    li.appendChild(document.createTextNode(banana.name + " " + banana.quantity + " " + banana.price));
+
+    var total_price = banana.total_price();
+    var total_price_formatted = total_price.toFixed(2);
+        
+    li.appendChild(document.createTextNode(banana.name + " " + banana.quantity + " " + "£" + total_price_formatted));
     ul.appendChild(li);
 
 }
 
 function quantity_plus_one_banana() {
     banana.quantity += 1;
-    document.getElementById("banana_li").textContent = banana.name + " " + banana.quantity + " " + banana.price;
+    var total_price = banana.total_price();
+    var total_price_formatted = total_price.toFixed(2);
+    document.getElementById("banana_li").textContent = banana.name + " " + banana.quantity + " " + "£" + total_price_formatted;
+}
+
+function quantity_minus_one_banana() {
+    banana.quantity -= 1;
+    var total_price = banana.total_price();
+    var total_price_formatted = total_price.toFixed(2);
+    document.getElementById("banana_li").textContent = banana.name + " " + banana.quantity + " " + "£" + total_price_formatted;
+}
+
+function remove_bananas_from_basket() {
+    var li = document.getElementById("banana_li");
+    var ul = document.getElementById("basket");
+    ul.removeChild(li);
 }
